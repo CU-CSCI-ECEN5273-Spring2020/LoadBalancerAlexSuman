aa8af3cb40f0a36aa7c0c85660263a01a63b7578
Alex Costinescu <paul.costinescu@gmail.com>
2020-03-14 18:50:48 -0600

    Multithreaded TCPServer-based balancer

--- balancer.py
+++ balancer.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python3
 
 from http import server
-import socketserver
 import requests
+
+request_num = 0
 
 # Request handler
 class BalancerHandler(server.BaseHTTPRequestHandler):
+     server_list = None
+
      def do_GET(self, body=True):
-          try:
-               hostname = "localhost:8000"
-               url = "http://{}{}".format(hostname, self.path)
-               req_header = self.parse_headers()
+          global request_num
 
-               resp = requests.get(url, headers=self.headers, verify=False)
+          # Select the server using round robin
+          server = self.select_roundrobin()
 
-               self.send_response(resp.status_code)
-               self.send_all_headers(resp.headers)
-               self.end_headers()
-               self.wfile.write(resp.content)
-          finally:
-               self.finish()
+          # Get the hostname from the server (hostname = address + port)
+          hostname = server["host"] + ":" + str(server["port"])
 
+          # Convert the hostname to a url (add http and request path)
+          url = "http://{}{}".format(hostname, self.path)
+
+          # Get the request headers
+          req_header = self.parse_headers()
+
+          # Send a request to the selected server
+          resp = requests.get(url, headers=self.headers, verify=False)
+
+          # Using the response from the selected server, send back all headers/content to client
+          self.send_response(resp.status_code)
+          self.send_all_headers(resp.headers)
+          self.end_headers()
+          self.wfile.write(resp.content)
+
+          # Increment the request number
+          request_num += 1
+               
      def send_all_headers(self, headers):
           for header in headers:
                parts = [o.strip() for o in header.split(':', 1)]
@@ -35,8 +50,18 @@ class BalancerHandler(server.BaseHTTPRequestHandler):
                     req_header[line_parts[0]] = line_parts[1]
           return req_header
 
+     def select_roundrobin(self):
+          server_num = request_num % len(self.server_list)
+          server = self.server_list[server_num]
+          return server
+
 class Balancer:
-     def start(address, port):
-          with socketserver.TCPServer((address, port), BalancerHandler) as httpd:
-               print("Serving at port", port)
+     def __init__(self, address, port, servers):
+
+          # Set the list of servers for the handler
+          BalancerHandler.server_list = servers
+
+          # Start the HTTP server
+          with server.HTTPServer((address, port), BalancerHandler) as httpd:
+               print("Load balancing at port", port)
                httpd.serve_forever()
--- run_balancer.py
+++ run_balancer.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3
+
 from balancer import Balancer
 
 ADDR = "localhost"
 PORT = 8080
+SERVERS = [
+               {"host": "localhost", "port": 8000}, 
+               {"host": "localhost", "port": 8001}
+          ]
 
-Balancer.start(ADDR, PORT)
+Balancer(ADDR, PORT, SERVERS)
