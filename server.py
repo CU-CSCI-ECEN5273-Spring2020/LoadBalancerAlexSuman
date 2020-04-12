aff713a75cbcf475c33f74656ceffebf60ce97af
sumanhosmane <suman.hosmane@colorado.edu>
2020-04-12 16:47:32 -0600

    addition of cpu_intensive to server.html

--- server.py
+++ server.py
@@ -1,14 +1,25 @@
 from http import server
+import request
 
 ADDR = "localhost"
 PORT = 8000
 
 class cpu_intenseive(server.BaseHTTPRequestHandler):
-    
-    
+    def compute_primes(self):
+        start_time = time.time()
+        num = random.randint(4589789,78909929)
+        num_copy = num
+        result = 0
+        for i in range(1,num):
+            result += i             #computing fiboncci series (exec time max 8sec)
+            #num_copy%i             #random division and mod operation (exec time 5sec)
+            #num_copy/i
+            end_time = time.time()
+        return (end_time - start_time)
     
     def do_GET(self,body = TRUE):
-        
+        exec_time = self.compute_primes()    
+        self.wfile.write(exec_time)
 
 httpd = server.HTTPServer((ADDR, PORT), cpu_intensive)
 httpd.serve_forever()
